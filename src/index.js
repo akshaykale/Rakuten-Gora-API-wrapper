ce4b3c7e20997e33ee0343321093f58c58f960b2
Akshay Kale <ark.akshaykale@gmail.com>
2019-02-22 14:27:54 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -125,7 +125,7 @@ moviesIntentHandler =  async (agent) => {
   //const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
   await callImdbApi(movieToSearch).then( async (movie) => {
     let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-    dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+    dataToSend += `${movie.Title} released in ${movie.Year}, is a ${movie.Actors} starer ${movie.Genre} movie. It was directed by ${movie.Director}`;
     console.log(dataToSend)
 
     const card = new Card(movie.Title);
